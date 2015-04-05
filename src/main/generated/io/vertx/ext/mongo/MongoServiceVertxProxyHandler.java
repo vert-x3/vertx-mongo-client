1e0f764fd8b5dce2b924a95907a63488fd932d49
Julien Viet <julien@julienviet.com>
2015-04-05 18:16:04 +0200

    Releasing 3.0.0-milestone4

--- src/main/generated/io/vertx/ext/mongo/MongoServiceVertxProxyHandler.java
+++ src/main/generated/io/vertx/ext/mongo/MongoServiceVertxProxyHandler.java
@@ -47,11 +47,44 @@ public class MongoServiceVertxProxyHandler extends ProxyHandler {
   private final Vertx vertx;
   private final MongoService service;
   private final String address;
+  private final long timerID;
+  private long lastAccessed;
+  private final long timeoutSeconds;
 
-  public MongoServiceVertxProxyHandler(Vertx vertx, MongoService service, String address) {
+  public MongoServiceVertxProxyHandler(Vertx vertx, MongoService service, String address, boolean topLevel, long timeoutSeconds) {
     this.vertx = vertx;
     this.service = service;
     this.address = address;
+    this.timeoutSeconds = timeoutSeconds;
+    if (timeoutSeconds != -1 && !topLevel) {
+      long period = timeoutSeconds * 1000 / 2;
+      if (period > 10000) {
+        period = 10000;
+      }
+      this.timerID = vertx.setPeriodic(period, this::checkTimedOut);
+    } else {
+      this.timerID = -1;
+    }
+    accessed();
+  }
+
+  private void checkTimedOut(long id) {
+    long now = System.nanoTime();
+    if (now - lastAccessed > timeoutSeconds * 1000000000) {
+      close();
+    }
+  }
+
+  @Override
+  public void close() {
+    if (timerID != -1) {
+      vertx.cancelTimer(timerID);
+    }
+    super.close();
+  }
+
+  private void accessed() {
+    this.lastAccessed = System.nanoTime();
   }
 
   public void handle(Message<JsonObject> msg) {
@@ -60,6 +93,7 @@ public class MongoServiceVertxProxyHandler extends ProxyHandler {
     if (action == null) {
       throw new IllegalStateException("action not specified");
     }
+    accessed();
     switch (action) {
 
 
