a86cd3d030ec7eb74e4ed2da5ecb551e4e8ab31a
Sergey Kobets <sergey.kobec@gmail.com>
2016-04-22 21:38:50 +0300

    index api
    Signed-off-by: Sergey Kobets <sergey.kobec@gmail.com>

--- vertx-mongo-client/src/main/java/io/vertx/ext/mongo/IndexOptions.java
+++ vertx-mongo-client/src/main/java/io/vertx/ext/mongo/IndexOptions.java
@@ -12,225 +12,225 @@ import java.util.concurrent.TimeUnit;
  */
 @DataObject
 public class IndexOptions {
-    private final com.mongodb.client.model.IndexOptions wrap;
+  private final com.mongodb.client.model.IndexOptions wrap;
 
-    public IndexOptions() {
-        wrap = new com.mongodb.client.model.IndexOptions();
-    }
+  public IndexOptions() {
+    wrap = new com.mongodb.client.model.IndexOptions();
+  }
 
-    public IndexOptions(IndexOptions other) {
-        wrap = other.unwrap();
-    }
+  public IndexOptions(IndexOptions other) {
+    wrap = other.unwrap();
+  }
 
-    // TODO not implemented
-    public IndexOptions(JsonObject json) {
-        wrap = new com.mongodb.client.model.IndexOptions();
-    }
+  // TODO not implemented
+  public IndexOptions(JsonObject json) {
+    wrap = new com.mongodb.client.model.IndexOptions();
+  }
 
-    // TODO not implemented
-    public JsonObject toJson(){
-        return new JsonObject();
-    }
+  // TODO not implemented
+  public JsonObject toJson() {
+    return new JsonObject();
+  }
 
-    public boolean isBackground() {
-        return wrap.isBackground();
-    }
+  public boolean isBackground() {
+    return wrap.isBackground();
+  }
 
-    public IndexOptions background(boolean background) {
-        wrap.background(background);
-        return this;
-    }
+  public IndexOptions background(boolean background) {
+    wrap.background(background);
+    return this;
+  }
 
 
-    public boolean isUnique() {
-        return wrap.isUnique();
-    }
+  public boolean isUnique() {
+    return wrap.isUnique();
+  }
 
 
-    public IndexOptions unique(boolean unique) {
-        wrap.unique(unique);
-        return this;
-    }
+  public IndexOptions unique(boolean unique) {
+    wrap.unique(unique);
+    return this;
+  }
 
 
-    public String getName() {
-        return wrap.getName();
-    }
+  public String getName() {
+    return wrap.getName();
+  }
 
 
-    public IndexOptions name(String name) {
-        wrap.name(name);
-        return this;
-    }
+  public IndexOptions name(String name) {
+    wrap.name(name);
+    return this;
+  }
 
 
-    public boolean isSparse() {
-        return wrap.isSparse();
-    }
+  public boolean isSparse() {
+    return wrap.isSparse();
+  }
 
 
-    public IndexOptions sparse(boolean sparse) {
-        wrap.sparse(sparse);
-        return this;
-    }
+  public IndexOptions sparse(boolean sparse) {
+    wrap.sparse(sparse);
+    return this;
+  }
 
 
-    public Long getExpireAfter(TimeUnit timeUnit) {
-        return wrap.getExpireAfter(timeUnit);
-    }
+  public Long getExpireAfter(TimeUnit timeUnit) {
+    return wrap.getExpireAfter(timeUnit);
+  }
 
 
-    public IndexOptions expireAfter(Long expireAfter, TimeUnit timeUnit) {
-        wrap.expireAfter(expireAfter, timeUnit);
-        return this;
-    }
+  public IndexOptions expireAfter(Long expireAfter, TimeUnit timeUnit) {
+    wrap.expireAfter(expireAfter, timeUnit);
+    return this;
+  }
 
 
-    public Integer getVersion() {
-        return wrap.getVersion();
-    }
+  public Integer getVersion() {
+    return wrap.getVersion();
+  }
 
 
-    public IndexOptions version(Integer version) {
-        wrap.version(version);
-        return this;
-    }
+  public IndexOptions version(Integer version) {
+    wrap.version(version);
+    return this;
+  }
 
 
-    public Bson getWeights() {
-        return wrap.getWeights();
-    }
+  public Bson getWeights() {
+    return wrap.getWeights();
+  }
 
-    // TODO migrate to json
-    public IndexOptions weights(Bson weights) {
-        wrap.weights(weights);
-        return this;
-    }
+  // TODO migrate to json
+  public IndexOptions weights(Bson weights) {
+    wrap.weights(weights);
+    return this;
+  }
 
 
-    public String getDefaultLanguage() {
-        return wrap.getDefaultLanguage();
-    }
+  public String getDefaultLanguage() {
+    return wrap.getDefaultLanguage();
+  }
 
 
-    public IndexOptions defaultLanguage(String defaultLanguage) {
-        wrap.defaultLanguage(defaultLanguage);
-        return this;
-    }
+  public IndexOptions defaultLanguage(String defaultLanguage) {
+    wrap.defaultLanguage(defaultLanguage);
+    return this;
+  }
 
 
-    public String getLanguageOverride() {
-        return wrap.getLanguageOverride();
-    }
+  public String getLanguageOverride() {
+    return wrap.getLanguageOverride();
+  }
 
 
-    public IndexOptions languageOverride(String languageOverride) {
-        wrap.languageOverride(languageOverride);
-        return this;
-    }
+  public IndexOptions languageOverride(String languageOverride) {
+    wrap.languageOverride(languageOverride);
+    return this;
+  }
 
 
-    public Integer getTextVersion() {
-        return wrap.getTextVersion();
-    }
+  public Integer getTextVersion() {
+    return wrap.getTextVersion();
+  }
 
 
-    public IndexOptions textVersion(Integer textVersion) {
-        wrap.textVersion(textVersion);
-        return this;
-    }
+  public IndexOptions textVersion(Integer textVersion) {
+    wrap.textVersion(textVersion);
+    return this;
+  }
 
 
-    public Integer getSphereVersion() {
-        return wrap.getSphereVersion();
-    }
+  public Integer getSphereVersion() {
+    return wrap.getSphereVersion();
+  }
 
 
-    public IndexOptions sphereVersion(Integer sphereVersion) {
-        wrap.sphereVersion(sphereVersion);
-        return this;
-    }
+  public IndexOptions sphereVersion(Integer sphereVersion) {
+    wrap.sphereVersion(sphereVersion);
+    return this;
+  }
 
 
-    public Integer getBits() {
-        return wrap.getBits();
-    }
+  public Integer getBits() {
+    return wrap.getBits();
+  }
 
 
-    public IndexOptions bits(Integer bits) {
-        wrap.bits(bits);
-        return this;
-    }
+  public IndexOptions bits(Integer bits) {
+    wrap.bits(bits);
+    return this;
+  }
 
 
-    public Double getMin() {
-        return wrap.getMin();
-    }
+  public Double getMin() {
+    return wrap.getMin();
+  }
 
 
-    public IndexOptions min(Double min) {
-        wrap.min(min);
-        return this;
-    }
+  public IndexOptions min(Double min) {
+    wrap.min(min);
+    return this;
+  }
 
 
-    public Double getMax() {
-        return wrap.getMax();
-    }
+  public Double getMax() {
+    return wrap.getMax();
+  }
 
 
-    public IndexOptions max(Double max) {
-        wrap.max(max);
-        return this;
-    }
+  public IndexOptions max(Double max) {
+    wrap.max(max);
+    return this;
+  }
 
 
-    public Double getBucketSize() {
-        return wrap.getBucketSize();
-    }
+  public Double getBucketSize() {
+    return wrap.getBucketSize();
+  }
 
 
-    public IndexOptions bucketSize(Double bucketSize) {
-        wrap.bucketSize(bucketSize);
-        return this;
-    }
+  public IndexOptions bucketSize(Double bucketSize) {
+    wrap.bucketSize(bucketSize);
+    return this;
+  }
 
 
-    public Bson getStorageEngine() {
-        return wrap.getStorageEngine();
-    }
+  public Bson getStorageEngine() {
+    return wrap.getStorageEngine();
+  }
 
-    // TODO migrate to json
-    public IndexOptions storageEngine(Bson storageEngine) {
-        wrap.storageEngine(storageEngine);
-        return this;
-    }
+  // TODO migrate to json
+  public IndexOptions storageEngine(Bson storageEngine) {
+    wrap.storageEngine(storageEngine);
+    return this;
+  }
 
 
-    public Bson getPartialFilterExpression() {
-        return wrap.getPartialFilterExpression();
-    }
+  public Bson getPartialFilterExpression() {
+    return wrap.getPartialFilterExpression();
+  }
 
-    // TODO migrate to json
-    public IndexOptions partialFilterExpression(Bson partialFilterExpression) {
-        wrap.partialFilterExpression(partialFilterExpression);
-        return this;
-    }
+  // TODO migrate to json
+  public IndexOptions partialFilterExpression(Bson partialFilterExpression) {
+    wrap.partialFilterExpression(partialFilterExpression);
+    return this;
+  }
 
-    public com.mongodb.client.model.IndexOptions unwrap() {
-        return wrap;
-    }
+  public com.mongodb.client.model.IndexOptions unwrap() {
+    return wrap;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        IndexOptions that = (IndexOptions) o;
-        return Objects.equals(wrap, that.wrap);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    IndexOptions that = (IndexOptions) o;
+    return Objects.equals(wrap, that.wrap);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(wrap);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(wrap);
+  }
 }
--- vertx-mongo-client/src/main/java/io/vertx/ext/mongo/Indexes.java
+++ vertx-mongo-client/src/main/java/io/vertx/ext/mongo/Indexes.java
@@ -2,30 +2,68 @@ package io.vertx.ext.mongo;
 
 import io.vertx.core.json.JsonObject;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 
 /**
  * Index builder
+ *
  * @author <a href="mailto:sergey.kobec@gmail.com">Sergey Kobets</a>
  */
 public class Indexes {
-    private Indexes() {
-    }
+  private Indexes() {
+  }
 
-    public static JsonObject ascending(List<String> fieldNames) {
-        requireNonNull(fieldNames, "fieldNames cannot be null");
-        JsonObject jsonObject;
-        return compoundIndex(fieldNames, 1);
-    }
+  public static JsonObject ascending(String... fieldNames) {
+    return ascending(Arrays.asList(fieldNames));
+  }
 
-    private static JsonObject compoundIndex(List<String> fieldNames, Object value) {
-        JsonObject document = new JsonObject();
-        for (String fieldName : fieldNames) {
-            document.put(fieldName, value);
-        }
-        return document;
-    }
+  public static JsonObject ascending(List<String> fieldNames) {
+    requireNonNull(fieldNames, "fieldNames cannot be null");
+    return compoundIndex(fieldNames, 1);
+  }
 
+  public static JsonObject descending(String... fieldNames) {
+    return descending(Arrays.asList(fieldNames));
+  }
+
+  public static JsonObject descending(List<String> fieldNames) {
+    requireNonNull(fieldNames, "fieldNames cannot be null");
+    return compoundIndex(fieldNames, -1);
+  }
+
+  public static JsonObject text(String fieldName) {
+    return new JsonObject().put(fieldName, "text");
+  }
+
+  public static JsonObject hashed(String fieldName) {
+    return new JsonObject().put(fieldName, "hashed");
+  }
+
+  public static JsonObject compoundIndex(JsonObject... indexes) {
+    return compoundIndex(Arrays.asList(indexes));
+  }
+
+
+  public static JsonObject compoundIndex(List<JsonObject> indexes) {
+    JsonObject jsonObject = new JsonObject();
+    for (JsonObject index : indexes) {
+      Set<String> strings = index.fieldNames();
+      for (String string : strings) {
+        jsonObject.put(string, index.getValue(string));
+      }
+    }
+    return jsonObject;
+  }
+
+  private static JsonObject compoundIndex(List<String> fieldNames, Object value) {
+    JsonObject document = new JsonObject();
+    for (String fieldName : fieldNames) {
+      document.put(fieldName, value);
+    }
+    return document;
+  }
 }
