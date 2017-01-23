492f7df058026208f714bf4e0ba17552085478f9
Julien Viet <julien@julienviet.com>
2017-01-23 09:30:40 +0100

    Update to parent pom 24 and enable generation of deprecated RxJava method

--- vertx-mongo-client/src/main/kotlin/io/vertx/kotlin/ext/mongo/IndexOptions.kt
+++ vertx-mongo-client/src/main/kotlin/io/vertx/kotlin/ext/mongo/IndexOptions.kt
@@ -3,7 +3,7 @@ package io.vertx.kotlin.ext.mongo
 import io.vertx.ext.mongo.IndexOptions
 
 fun IndexOptions(
-    ): IndexOptions = io.vertx.ext.mongo.IndexOptions().apply {
+  ): IndexOptions = io.vertx.ext.mongo.IndexOptions().apply {
 
 }
 
--- vertx-mongo-client/src/main/kotlin/io/vertx/kotlin/ext/mongo/MongoClientUpdateResult.kt
+++ vertx-mongo-client/src/main/kotlin/io/vertx/kotlin/ext/mongo/MongoClientUpdateResult.kt
@@ -3,7 +3,7 @@ package io.vertx.kotlin.ext.mongo
 import io.vertx.ext.mongo.MongoClientUpdateResult
 
 fun MongoClientUpdateResult(
-    ): MongoClientUpdateResult = io.vertx.ext.mongo.MongoClientUpdateResult().apply {
+  ): MongoClientUpdateResult = io.vertx.ext.mongo.MongoClientUpdateResult().apply {
 
 }
 
