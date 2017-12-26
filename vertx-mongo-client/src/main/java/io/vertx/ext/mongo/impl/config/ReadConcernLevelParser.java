b0f495d2f68d6ba562293ee101f45bea2c24b9ba
slnowak <slnowak154@gmail.com>
2017-12-26 23:04:17 +0100

    Added support for reading ReadConcernLevel

---
+++ vertx-mongo-client/src/main/java/io/vertx/ext/mongo/impl/config/ReadConcernLevelParser.java
@@ -0,0 +1,38 @@
+package io.vertx.ext.mongo.impl.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadConcernLevel;
+import io.vertx.core.json.JsonObject;
+
+import java.util.Optional;
+
+class ReadConcernLevelParser {
+
+  private final ConnectionString connectionString;
+  private final JsonObject config;
+
+  ReadConcernLevelParser(ConnectionString connectionString, JsonObject config) {
+    this.connectionString = connectionString;
+    this.config = config;
+  }
+
+  Optional<ReadConcern> readConcern() {
+    return tryToParseFromConnectionString().map(this::lift).orElseGet(this::tryToParseFromConfig);
+  }
+
+  private Optional<ReadConcern> lift(ReadConcern readConcern) {
+    return Optional.ofNullable(readConcern);
+  }
+
+  private Optional<ReadConcern> tryToParseFromConnectionString() {
+    return Optional.ofNullable(connectionString).map(ConnectionString::getReadConcern);
+  }
+
+  private Optional<ReadConcern> tryToParseFromConfig() {
+    return Optional.ofNullable(config)
+      .flatMap(cfg -> Optional.ofNullable(cfg.getString("readConcernLevel")))
+      .map(ReadConcernLevel::fromString)
+      .map(ReadConcern::new);
+  }
+}
