96d223f4dfa5d6bf56ea34c46a5c8022f81b7e55
Lukas Prettenthaler <lukas@noenv.com>
2019-05-03 12:35:30 +0200

    add caPath option for trustManager, switch to vertx helper for trustAll
    Signed-off-by: Lukas Prettenthaler <lukas@noenv.com>

--- src/test/java/io/vertx/ext/mongo/impl/config/ParsingSSLOptionsTest.java
+++ src/test/java/io/vertx/ext/mongo/impl/config/ParsingSSLOptionsTest.java
@@ -3,11 +3,20 @@ package io.vertx.ext.mongo.impl.config;
 import com.mongodb.async.client.MongoClientSettings;
 import com.mongodb.connection.SslSettings;
 import io.vertx.core.json.JsonObject;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class ParsingSSLOptionsTest {
+
+  @Rule
+  public TemporaryFolder tmpFolder = new TemporaryFolder();
 
   @Test
   public void ssl_should_be_disabled_by_default() {
@@ -96,5 +105,86 @@ public class ParsingSSLOptionsTest {
 
     // then
     assertNotNull(sslSettings.getContext());
+  }
+
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidCaPathProperty() {
+    // given
+    final JsonObject withSSLAndCaPath = new JsonObject()
+      .put("ssl", true)
+      .put("caPath", "notExisting.pem");
+
+    // then
+    new MongoClientOptionsParser(withSSLAndCaPath);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testEmptyPemCertificate() throws IOException {
+    // given
+    final File tmpFile = tmpFolder.newFile("invalidCa.pem");
+    final JsonObject withSSLAndCaPath = new JsonObject()
+      .put("ssl", true)
+      .put("caPath", tmpFile.getAbsolutePath());
+
+    // when
+    final SslSettings sslSettings = new MongoClientOptionsParser(withSSLAndCaPath)
+      .settings()
+      .getSslSettings();
+
+    // then
+    assertNotNull(sslSettings.getContext());
+  }
+
+  @Test
+  public void testValidPemCertificate() throws IOException {
+    // given
+    final File tmpFile = tmpFolder.newFile("validCa.pem");
+    try (final FileWriter tmpWriter = new FileWriter(tmpFile)) {
+      tmpWriter.write("-----BEGIN CERTIFICATE-----\n" +
+        "MIICljCCAfigAwIBAgIJAK0oe+f4DaojMAoGCCqGSM49BAMEMFkxCzAJBgNVBAYT\n" +
+        "AkFUMQ8wDQYDVQQIDAZWaWVubmExDjAMBgNVBAoMBU5vRW52MSkwJwYDVQQLDCBO\n" +
+        "b0VudiBSb290IENlcnRpZmljYXRlIEF1dGhvcml0eTAeFw0xNjEwMjcxNTAwNTFa\n" +
+        "Fw00NjEwMjAxNTAwNTFaMFkxCzAJBgNVBAYTAkFUMQ8wDQYDVQQIDAZWaWVubmEx\n" +
+        "DjAMBgNVBAoMBU5vRW52MSkwJwYDVQQLDCBOb0VudiBSb290IENlcnRpZmljYXRl\n" +
+        "IEF1dGhvcml0eTCBmzAQBgcqhkjOPQIBBgUrgQQAIwOBhgAEAHpsMQth12N0d+aE\n" +
+        "FIFRd8in4MTYZNSQEyQ4fuPDNq0Zb+4TXpUmedLZQJKkAQxorak8ESC/tXuQJDUL\n" +
+        "OoKa+R6NAT4EKR1aaVVd7clC9rfGqVwGYslppycy9zsN6O4XLUiripamQF78FzRF\n" +
+        "8wRZvkwYhzud+jpV6shgEMw3zmcwDSYKo2YwZDAdBgNVHQ4EFgQUD96n//91CReu\n" +
+        "Cz1K0qics6aNFV0wHwYDVR0jBBgwFoAUD96n//91CReuCz1K0qics6aNFV0wEgYD\n" +
+        "VR0TAQH/BAgwBgEB/wIBATAOBgNVHQ8BAf8EBAMCAYYwCgYIKoZIzj0EAwQDgYsA\n" +
+        "MIGHAkFOxsApSB7fn8ZnYG/EUscn/uAkjxHsvdEkPKCC+XYCKMssW4YP2kR6gZjo\n" +
+        "J8vaOAJZwNevBe/R9J8zMvsAWRJmWgJCAKLedGLnBuJOK9jjnKBwbVm5OIQfApMA\n" +
+        "I2mJVnNXvS12w4DTZlP0K1t63WxsykBBTOIVXnYdPkdZvvnoAIcfA7iM\n" +
+        "-----END CERTIFICATE-----");
+    }
+    final JsonObject withSSLAndCaPath = new JsonObject()
+      .put("ssl", true)
+      .put("caPath", tmpFile.getAbsolutePath());
+
+    // when
+    final SslSettings sslSettings = new MongoClientOptionsParser(withSSLAndCaPath)
+      .settings()
+      .getSslSettings();
+
+    // then
+    assertNotNull(sslSettings.getContext());
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testInvalidPemCertificate() throws IOException {
+    // given
+    final File tmpFile = tmpFolder.newFile("validCa.pem");
+    try (final FileWriter tmpWriter = new FileWriter(tmpFile)) {
+      tmpWriter.write("-----BEGIN CERTIFICATE-----\n" +
+        "MIICljCCAfigAwIBAgI...BROKEN...xsykBBTOIVXnYdPkdZvvnoAIcfA7iM\n" +
+        "-----END CERTIFICATE-----");
+    }
+    final JsonObject withSSLAndCaPath = new JsonObject()
+      .put("ssl", true)
+      .put("caPath", tmpFile.getAbsolutePath());
+
+    // then
+    new MongoClientOptionsParser(withSSLAndCaPath);
   }
 }
