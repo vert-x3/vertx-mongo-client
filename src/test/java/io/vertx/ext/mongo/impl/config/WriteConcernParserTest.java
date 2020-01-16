6065430f6e38f895a7739d52f647c07a68612e38
Thomas Segismont <tsegismo@redhat.com>
2020-01-16 16:41:43 +0100

    Fixed failing parser tests

--- src/test/java/io/vertx/ext/mongo/impl/config/WriteConcernParserTest.java
+++ src/test/java/io/vertx/ext/mongo/impl/config/WriteConcernParserTest.java
@@ -48,12 +48,12 @@ public class WriteConcernParserTest {
     new WriteConcernParser(null, config).writeConcern();
   }
 
-  @Test(expected = ClassCastException.class)
+  @Test(expected = IllegalArgumentException.class)
   public void testInvalidTypeWriteConcern() {
     JsonObject config = new JsonObject();
     config.put("writeConcern", 123);
 
-    new WriteConcernParser(null, config).writeConcern();
+    new WriteConcernParser(null, config);
   }
 
   @Test
