7f2376bdee05956559d1caa02dd75a8bbba59eb1
Lukas Prettenthaler <rdx@wsn.at>
2020-05-14 23:28:06 +0200

    add client certificate authentication
    Signed-off-by: Lukas Prettenthaler <rdx@wsn.at>

--- src/main/java/io/vertx/ext/mongo/impl/config/SSLSettingsParser.java
+++ src/main/java/io/vertx/ext/mongo/impl/config/SSLSettingsParser.java
@@ -2,29 +2,22 @@ package io.vertx.ext.mongo.impl.config;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.connection.SslSettings;
-import io.vertx.core.impl.logging.Logger;
-import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.Vertx;
+import io.vertx.core.impl.VertxInternal;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.core.net.PemKeyCertOptions;
+import io.vertx.core.net.PemTrustOptions;
+import io.vertx.core.net.impl.KeyStoreHelper;
 import io.vertx.core.net.impl.TrustAllTrustManager;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.util.Optional;
+import javax.net.ssl.*;
+import java.security.*;
 
 /**
  * @author <a href="mailto:nscavell@redhat.com">Nick Scavelli</a>
+ * @author Lukas Prettenthaler
  */
 class SSLSettingsParser {
   private static final Logger log = LoggerFactory.getLogger(SSLSettingsParser.class);
@@ -36,60 +29,55 @@ class SSLSettingsParser {
     this.config = config;
   }
 
-  public SslSettings settings() {
-    final SslSettings.Builder builder = fromConnectionString().orElseGet(this::fromConfiguration);
-    if (config.getBoolean("trustAll", false)) {
-      log.warn("Mongo client has been set to trust ALL certificates, this can open you up to security issues. Make sure you know the risks.");
-      try {
-        final SSLContext context = SSLContext.getInstance("TLS");
-        context.init(null, new TrustManager[]{TrustAllTrustManager.INSTANCE}, new SecureRandom());
-        builder.context(context);
-      } catch (final NoSuchAlgorithmException | KeyManagementException e) {
-        //fail silently on error
-      }
+  public SslSettings settings(Vertx vertx) {
+    final SslSettings.Builder builder = SslSettings.builder();
+    fromConnectionString(builder);
+    fromConfiguration(builder);
+
+    final SslSettings settings = builder.build();
+    if (!settings.isEnabled()) {
+      return settings;
     }
+    final PemKeyCertOptions pemKeyCertOptions = new PemKeyCertOptions();
+    final PemTrustOptions pemTrustOptions = new PemTrustOptions();
     if (config.containsKey("caPath")) {
-      final String caPath = config.getString("caPath");
-      try {
-        final TrustManagerFactory tmf = buildTrustManagerFactory(caPath);
-        final SSLContext context = SSLContext.getInstance("TLS");
-        context.init(null, tmf.getTrustManagers(), new SecureRandom());
-        builder.context(context);
-      } catch (final FileNotFoundException e) {
-        throw new IllegalArgumentException("Invalid caPath " + e.getMessage());
-      } catch (final NoSuchAlgorithmException | CertificateException | KeyStoreException | IOException | KeyManagementException e) {
-        throw new IllegalArgumentException("Unable to load certificate from caPath '" + caPath + "' " + e.getMessage());
+      pemTrustOptions.addCertPath(config.getString("caPath"));
+    }
+    if (config.containsKey("keyPath") && config.containsKey("certPath")) {
+      pemKeyCertOptions.addKeyPath(config.getString("keyPath"));
+      pemKeyCertOptions.addCertPath(config.getString("certPath"));
+    }
+    try {
+      final TrustManager[] tms;
+      if (config.getBoolean("trustAll", false)) {
+        log.warn("Mongo client has been set to trust ALL certificates, this can open you up to security issues. Make sure you know the risks.");
+        tms = new TrustManager[]{TrustAllTrustManager.INSTANCE};
+      } else {
+        final KeyStoreHelper pemTrustStore = KeyStoreHelper.create((VertxInternal) vertx, pemTrustOptions);
+        tms = pemTrustStore.getTrustMgrs((VertxInternal) vertx);
       }
+      final KeyStoreHelper pemKeyCertStore = KeyStoreHelper.create((VertxInternal) vertx, pemKeyCertOptions);
+      final SSLContext context = SSLContext.getInstance("TLS");
+      context.init(pemKeyCertStore.getKeyMgr(), tms, new SecureRandom());
+      builder.context(context);
+    } catch (final Exception e) {
+      throw new IllegalArgumentException(e);
     }
     return builder.build();
   }
 
-  private Optional<SslSettings.Builder> fromConnectionString() {
-    return Optional.ofNullable(connectionString).map(cs ->
-      SslSettings.builder()
-        .applyConnectionString(cs)
-    );
+  private void fromConnectionString(SslSettings.Builder builder) {
+    if (connectionString != null) {
+      builder.applyConnectionString(connectionString);
+    }
   }
 
-  private SslSettings.Builder fromConfiguration() {
-    return SslSettings.builder()
-      .enabled(config.getBoolean("ssl", false))
-      .invalidHostNameAllowed(config.getBoolean("sslInvalidHostNameAllowed", false));
-  }
-
-  private static TrustManagerFactory buildTrustManagerFactory(final String caPath)
-    throws NoSuchAlgorithmException, CertificateException, KeyStoreException, IOException {
-    final CertificateFactory fact = CertificateFactory.getInstance("X.509");
-    final FileInputStream is = new FileInputStream(caPath);
-    final X509Certificate cert = (X509Certificate) fact.generateCertificate(is);
-
-    final KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-    ks.load(null, null);
-    ks.setCertificateEntry("1", cert);
-
-    final TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-    trustManagerFactory.init(ks);
-
-    return trustManagerFactory;
+  private void fromConfiguration(SslSettings.Builder builder) {
+    if (config.containsKey("ssl")) {
+      builder.enabled(config.getBoolean("ssl", false));
+    }
+    if (config.containsKey("sslInvalidHostNameAllowed")) {
+      builder.invalidHostNameAllowed(config.getBoolean("sslInvalidHostNameAllowed", false));
+    }
   }
 }
--- src/test/java/io/vertx/ext/mongo/impl/config/MongoClientOptionsParserTest.java
+++ src/test/java/io/vertx/ext/mongo/impl/config/MongoClientOptionsParserTest.java
@@ -1,17 +1,32 @@
 package io.vertx.ext.mongo.impl.config;
 
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MongoClientOptionsParserTest {
+  private Vertx vertx;
+
+  @Before
+  public void setUp() {
+    vertx = Vertx.vertx();
+  }
+
+  @After
+  public void tearDown() {
+    vertx.close();
+  }
+
   @Test
   public void testConnStringDbName() {
     String connectionString = "mongodb://localhost:27017/mydb";
     JsonObject config = new JsonObject().put("connection_string", connectionString).put("db_name", "unused_db");
 
-    MongoClientOptionsParser parser = new MongoClientOptionsParser(config);
+    MongoClientOptionsParser parser = new MongoClientOptionsParser(vertx, config);
     assertEquals("mydb", parser.database());
   }
 
@@ -20,7 +35,7 @@ public class MongoClientOptionsParserTest {
     String connectionString = "mongodb://localhost:27017/";
     JsonObject config = new JsonObject().put("connection_string", connectionString).put("db_name", "my_db");
 
-    MongoClientOptionsParser parser = new MongoClientOptionsParser(config);
+    MongoClientOptionsParser parser = new MongoClientOptionsParser(vertx, config);
     assertEquals("my_db", parser.database());
   }
 }
