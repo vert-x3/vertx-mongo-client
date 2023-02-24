b2a5c34180300ab88189baeb9aa20950f9ae3eba
刘冲 <mail@liuchong.io>
2023-02-24 19:13:05 +0800

    Add test for check collection type timeseries when use TimeSeriesOptions

--- src/test/java/io/vertx/ext/mongo/TimeSeriesOptionsTest.java
+++ src/test/java/io/vertx/ext/mongo/TimeSeriesOptionsTest.java
@@ -4,11 +4,26 @@ import static org.junit.Assert.*;
 
 import com.mongodb.client.model.TimeSeriesGranularity;
 import io.vertx.core.json.JsonObject;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import org.junit.Test;
 
-public class TimeSeriesOptionsTest {
+public class TimeSeriesOptionsTest extends MongoTestBase {
+
+  protected MongoClient mongoClient;
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    JsonObject config = getConfig();
+    mongoClient = MongoClient.create(vertx, config);
+    CountDownLatch latch = new CountDownLatch(1);
+    dropCollections(mongoClient, latch);
+    awaitLatch(latch);
+  }
 
   private static void assertNotEqual(BiConsumer<TimeSeriesOptions, TimeSeriesOptions> f) {
     TimeSeriesOptions a = new TimeSeriesOptions("time");
@@ -101,5 +116,42 @@ public class TimeSeriesOptionsTest {
 
     assertEquals(
         TimeSeriesGranularity.SECONDS, options4.setGranularity("seconds").getGranularity());
+  }
+
+  @Test
+  public void testCreateTimeSeriesCollection() {
+    final String collectionName = "_testCreateTimeSeriesCollection" + UUID.randomUUID();
+    TimeSeriesOptions timeseries =
+        new TimeSeriesOptions(
+            JsonObject.of(
+                TimeSeriesOptions.TIME_FIELD_KEY,
+                "timestamp",
+                TimeSeriesOptions.META_FIELD_KEY,
+                "metadata",
+                TimeSeriesOptions.GRANULARITY_KEY,
+                "hours"));
+    CreateCollectionOptions options = new CreateCollectionOptions();
+    options.setTimeseries(timeseries);
+    mongoClient
+        .createCollectionWithOptions(collectionName, options)
+        .onSuccess(
+            _void -> {
+              mongoClient
+                  .runCommand("listCollections", JsonObject.of("listCollections", "1.0"))
+                  .onSuccess(
+                      json -> {
+                        boolean isTimeSeriesCollection = false;
+                        for (Object obj : json.getJsonObject("cursor").getJsonArray("firstBatch")) {
+                          JsonObject coll = (JsonObject) obj;
+                          if (Objects.equals(collectionName, coll.getString("name"))) {
+                            if (Objects.equals("timeseries", coll.getString("type"))) {
+                              isTimeSeriesCollection = true;
+                            }
+                          }
+                        }
+                        assert isTimeSeriesCollection;
+                      });
+            });
+    await();
   }
 }
