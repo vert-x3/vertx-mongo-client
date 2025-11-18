cda559091277b23614cd0a380c257ae8aded5d88
Peter Ferak <Peter.Ferak@5gsystems.com>
2025-11-18 18:57:01 +0100

    test(transaction-insert): add test class with transaction client, insert into 2 collection test

---
+++ src/test/java/io/vertx/ext/mongo/tests/MongoClientWithTransactionTest.java
@@ -0,0 +1,66 @@
+package io.vertx.ext.mongo.tests;
+
+import com.mongodb.MongoClientSettings;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.mongo.impl.config.MongoClientOptionsParser;
+import org.junit.Test;
+
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+
+public class MongoClientWithTransactionTest extends MongoClientTestBase {
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    JsonObject config = getConfig();
+    config.put("useObjectId", true);
+    useObjectId = true;
+    final MongoClientSettings settings = new MongoClientOptionsParser(vertx, config).settings();
+    mongoClient = MongoClient.createWithMongoSettings(vertx, config, UUID.randomUUID().toString(), settings);
+    CountDownLatch latch = new CountDownLatch(1);
+    dropCollections(mongoClient, latch);
+    awaitLatch(latch);
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    mongoClient.close();
+    super.tearDown();
+  }
+
+  protected static JsonObject getConfig() {
+    JsonObject config = MongoClientTestBase.getConfig();
+    config.put("useObjectId", true);
+    return config;
+  }
+
+  @Test
+  public void testReplaceUpsert() {
+    String collection = randomCollection();
+    String collection2 = randomCollection();
+
+    mongoClient.createTransactionContext().onComplete(
+      onSuccess(client -> {
+        client.start();
+        JsonObject doc = createDoc();
+        JsonObject doc2 = createDoc();
+
+        client.insert(collection, doc).onComplete(onSuccess(id -> {
+          assertNotNull(id);
+
+          client.insert(collection2, doc2).onComplete(onSuccess(id2 -> {
+            assertNotNull(id2);
+            client.commit()
+              .onComplete(onSuccess(v -> {
+                assertNull(v);
+                testComplete();
+              }));
+          }));
+        }));
+      }));
+
+    await();
+  }
+}
